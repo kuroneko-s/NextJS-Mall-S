a82f456ebeef064dd53f5e2c09fe3585c7d9a2e9
kuroneko <drivespublic@gmail.com>
2023-05-02 20:34:47 +0900

    이미지 슬라이드 수정.

--- components/main/event/index.tsx
+++ components/main/event/index.tsx
@@ -1,26 +1,76 @@
 import { cls } from "@lib/client/common";
 import React, { useState, useRef, useEffect } from "react";
+import styled from "styled-components";
+
+interface SlideProps {
+  width: number;
+  transitionSwitch?: boolean;
+  left?: number;
+}
+
+const SlideContainer = styled.div<SlideProps>`
+  display: flex;
+  position: relative;
+  height: 280px;
+  width: ${(props) => props.width}px;
+  left: ${(props) => props.left}px;
+
+  ${(props) =>
+    props.transitionSwitch
+      ? `transition-property: all;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+  transition-duration: 150ms;
+  transition-duration: 700ms;`
+      : ""}
+`;
+
+const SlideBox = styled.div<SlideProps>`
+  width: ${(props) => props.width}px;
+  height: 100%;
+  background-color: rgb(252 165 165);
+  border-radius: 0.375rem;
+`;
 
 export default function Event() {
-  const defaultWith = useRef(1280);
+  // const defaultWith = useRef(1280);
+  const FLAG_ELEMENT = useRef<HTMLDivElement>(null);
   const MAX_PAGE = 9;
   const MIN_PAGE = 1;
-  const MAXIMUM_LEFT = defaultWith.current * -9;
-  const [currentPage, setCurrentPage] = useState(1);
-  const [left, setLeft] = useState<number>(defaultWith.current * -1);
+
+  const [defaultWith, setDefaultWith] = useState<number>(1280);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [left, setLeft] = useState<number>(defaultWith * -1);
   const [transitionSwitch, setTransitionSwitch] = useState<boolean>(true);
+
+  const MAXIMUM_LEFT = defaultWith * -9;
+
+  useEffect(() => {
+    if (FLAG_ELEMENT.current !== null) {
+      setDefaultWith(FLAG_ELEMENT.current.offsetWidth);
+      setLeft(defaultWith * -1);
+    }
+
+    window.addEventListener("resize", resizeHandler);
+    return () => window.removeEventListener("resize", resizeHandler);
+  }, [defaultWith]);
+
+  const resizeHandler = () => {
+    if (FLAG_ELEMENT.current !== null) {
+      setDefaultWith(FLAG_ELEMENT.current.offsetWidth);
+    }
+  };
 
   const nextBtnHandler = () => {
     if (currentPage !== MAX_PAGE) {
-      setLeft((cur) => cur - defaultWith.current);
+      setLeft((cur) => cur - defaultWith);
       setCurrentPage((cur) => ++cur);
     } else if (currentPage === MAX_PAGE) {
-      setLeft((cur) => cur - defaultWith.current);
+      setLeft((cur) => cur - defaultWith);
       setCurrentPage((cur) => ++cur);
 
       setTimeout(() => {
         setTransitionSwitch(false);
-        setLeft(defaultWith.current * -1);
+        setLeft(defaultWith * -1);
         setCurrentPage(1);
       }, 700);
 
@@ -32,10 +82,10 @@ export default function Event() {
 
   const previousBtnHandler = () => {
     if (currentPage !== MIN_PAGE) {
-      setLeft((cur) => cur + defaultWith.current);
+      setLeft((cur) => cur + defaultWith);
       setCurrentPage((cur) => --cur);
     } else if (currentPage === MIN_PAGE) {
-      setLeft((cur) => cur + defaultWith.current);
+      setLeft((cur) => cur + defaultWith);
       setCurrentPage((cur) => --cur);
 
       setTimeout(() => {
@@ -51,26 +101,25 @@ export default function Event() {
   };
 
   return (
-    <div className="w-full min-h-screen space-y-14 overflow-hidden">
-      <div
-        className={cls(
-          "flex relative w-[14080px] h-[280px]",
-          transitionSwitch ? "transition-all duration-700" : ""
-        )}
-        style={{
-          left: `${left}px`,
-        }}
+    <div
+      className="w-full min-h-screen space-y-14 overflow-hidden"
+      ref={FLAG_ELEMENT}
+    >
+      <SlideContainer
+        left={left}
+        transitionSwitch={transitionSwitch}
+        width={defaultWith * 11}
       >
-        <div className="w-[1280px] h-full bg-red-300 rounded-md">0</div>
+        <SlideBox width={defaultWith}>0</SlideBox>
         {new Array(9).fill(1).map((_, idx) => {
           return (
-            <div key={idx} className="w-[1280px] h-full bg-red-300 rounded-md">
+            <SlideBox key={idx} width={defaultWith}>
               {idx + 1}
-            </div>
+            </SlideBox>
           );
         })}
-        <div className="w-[1280px] h-full bg-red-300 rounded-md">10</div>
-      </div>
+        <SlideBox width={defaultWith}>10</SlideBox>
+      </SlideContainer>
       <div className="flex justify-between">
         <button className="bg-slate-400 py-3 px-6" onClick={previousBtnHandler}>
           이전

a5334047caf06243a658aa900d7a57d3868d197b
kuroneko <drivespublic@gmail.com>
2023-05-27 16:18:51 +0900

    검색 기능 추가.

--- components/menu/Search.tsx
+++ components/menu/Search.tsx
@@ -1,21 +1,25 @@
 import ReadingGlassesSvg from "@svg/ReadingGlasses";
 import Image from "next/image";
 import Link from "next/link";
-import React, { KeyboardEvent, useRef, useState } from "react";
+import React, { KeyboardEvent, useEffect, useRef, useState } from "react";
 import emptyImg from "@images/empty.jpg";
 import { server } from "@lib/common";
 import { Book } from "@prisma/client";
-import { cls } from "@lib/client/common";
 
 export default function Search() {
   const [keyword, setKeyword] = useState<string>("");
   const [bookList, setBookList] = useState<Book[]>();
-  const [active, setActive] = useState<boolean>(false);
 
   const inputRef = useRef<HTMLInputElement>(null);
-  const searchListBoxRef = useRef<HTMLInputElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const searchListRef = useRef<HTMLDivElement>(null);
+  const searchListHeight = useRef<number>(350);
+  const currentPage = useRef<number>(1);
+  const isLoading = useRef<boolean>(false);
+  const searchBookListTotalCnt = useRef<number>(0);
 
   const searchHandler = () => {
+    inputRef.current?.focus();
     search();
   };
 
@@ -26,17 +30,40 @@ export default function Search() {
   };
 
   const search = async () => {
-    const bookList = await fetch(`${server}/api/db/getBookListForTitle`, {
+    const _bookList = await fetch(`${server}/api/db/getBookListForTitle`, {
       method: "POST",
-      body: JSON.stringify({ keyword }),
+      body: JSON.stringify({ keyword, currentPage: currentPage.current }),
     }).then((res) => res.json());
 
-    setBookList(bookList.data);
-    setActive(true);
+    searchBookListTotalCnt.current = _bookList.count ?? 0;
+
+    setBookList((cur) => {
+      const result = [...(cur ?? []), ...(_bookList?.data ?? [])];
+      searchListHeight.current =
+        result.length === 0 ? 110 : result.length * 110;
+
+      isLoading.current = false;
+      return result;
+    });
+  };
+
+  const scrollHandler = (e: React.UIEvent<HTMLElement>) => {
+    const now = e.currentTarget.scrollTop;
+    const percent = now / searchListHeight.current;
+
+    if (
+      (bookList?.length ?? 1) < searchBookListTotalCnt.current &&
+      percent >= 0.6 &&
+      !isLoading.current
+    ) {
+      currentPage.current = currentPage.current + 1;
+      isLoading.current = true;
+      search();
+    }
   };
 
   return (
-    <div className="flex flex-col items-end group" ref={searchListBoxRef}>
+    <div className="flex flex-col items-end group" ref={containerRef}>
       <div className="relative">
         <div className="cursor-pointer" onClick={searchHandler}>
           <ReadingGlassesSvg />
@@ -46,17 +73,23 @@ export default function Search() {
           type={"text"}
           value={keyword}
           maxLength={50}
-          onChange={(e) => setKeyword(e.target.value)}
+          onChange={(e) => {
+            setKeyword(e.target.value);
+            setBookList(undefined);
+            currentPage.current = 1;
+          }}
           onKeyDown={keyDownHandler}
-          className="bg-gray-200 h-full pl-8 py-2 shadow-sm rounded-lg w-[220px] outline-none focus:ring-offset-1 focus:ring-2 focus:ring-blue-500 group-focus-within:w-[300px] duration-150 ease-in"
+          className="bg-gray-200 h-full pl-8 py-2 shadow-sm rounded-lg w-[220px] outline-none focus:ring-offset-1 focus:ring-2 focus:ring-blue-500 group-focus-within:w-[300px] delay-75 duration-150 ease-in"
         />
       </div>
       <div
         className={
           "absolute top-[95px] flex-col z-10 max-h-[380px] overflow-y-scroll w-[300px] space-y-3 bg-slate-50 rounded-md shadow-md px-1 transition delay-50 duration-100 ease-in-out group-focus-within:flex hidden"
         }
+        ref={searchListRef}
+        onScroll={scrollHandler}
       >
-        {bookList === undefined || bookList?.length <= 0 ? (
+        {bookList === undefined ? null : bookList.length <= 0 ? (
           <div>검색결과 없음</div>
         ) : (
           bookList.map((book) => {
@@ -66,9 +99,9 @@ export default function Search() {
                   className="hover:bg-slate-200 flex"
                   onClick={() => {
                     setKeyword("");
-                    setActive(false);
+                    setBookList(undefined);
                     inputRef.current?.blur();
-                    searchListBoxRef.current?.blur();
+                    containerRef.current?.blur();
                   }}
                 >
                   <div className="w-[80px] h-[110px] rounded-sm overflow-hidden mr-1">
--- pages/api/db/getBookListForTitle.ts
+++ pages/api/db/getBookListForTitle.ts
@@ -1,4 +1,5 @@
 import prismaClient from "@lib/server/prismaClient";
+import { Book } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
@@ -11,7 +12,9 @@ export default async function handler(
     });
   }
 
-  const keyword = JSON.parse(req.body)?.keyword ?? "";
+  const body = JSON.parse(req.body);
+  const keyword = body?.keyword ?? "";
+  const currentPage = body.currentPage ?? "1";
 
   if (keyword === "") {
     return res.json({
@@ -19,12 +22,21 @@ export default async function handler(
     });
   }
 
-  const bookList = await prismaClient.$queryRawUnsafe(
+  const limitStart = (currentPage - 1) * 10;
+  const limitEnd = 10;
+
+  const bookList: Book[] = await prismaClient.$queryRawUnsafe(
+    `SELECT * FROM Book WHERE title LIKE '%${keyword}%' ORDER BY createDt LIMIT ${limitStart}, ${limitEnd}`
+  );
+
+  // TODO : Prisma 기능으로 LIKE 지원하는게 없어서queryRawUnsafe 사용중인데 count 값을 못가져옴. 다른 방법 있는지 확인 필요.
+  const allBookList: Book[] = await prismaClient.$queryRawUnsafe(
     `SELECT * FROM Book WHERE title LIKE '%${keyword}%'`
   );
 
   return res.json({
     ok: true,
     data: bookList,
+    count: allBookList.length,
   });
 }
